864d7c7c554291a53b12c060f117ef1fb39d25d1
satyam <2019335@iiitdmj.ac.in.com>
2021-01-06 14:48:52 +0530

    Initial Commit

--- app/src/main/java/com/example/sticknotes/MainActivity.java
+++ app/src/main/java/com/example/sticknotes/MainActivity.java
@@ -1,14 +1,73 @@
 package com.example.sticknotes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    SharedPreferences sharedPreferences;
+
+    ArrayList<String> notes = new ArrayList<String>();
+
+   // public void AddText(){
+   //     String s;
+
+    //}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_bar,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+         super.onOptionsItemSelected(item);
+         switch (item.getItemId()){
+             case R.id.add:
+             {
+                 //AddText();
+                 return true;
+             }
+             default:
+                 return false;
+         }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.notes);
+        sharedPreferences = this.getSharedPreferences("com.example.sticknotes", Context.MODE_PRIVATE);
+        notes.add("Tap to Add Your Notes");
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent();
+                
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/sticknotes/NotesEditor.java
@@ -0,0 +1,14 @@
+package com.example.sticknotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NotesEditor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes_editor);
+    }
+}
