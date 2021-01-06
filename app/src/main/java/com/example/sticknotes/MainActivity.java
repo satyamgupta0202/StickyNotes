cf7367a7e7382f1feab46186bae037b82c9c86d0
satyam <2019335@iiitdmj.ac.in.com>
2021-01-06 22:43:45 +0530

    Initial Commit

--- app/src/main/java/com/example/sticknotes/MainActivity.java
+++ app/src/main/java/com/example/sticknotes/MainActivity.java
@@ -1,16 +1,17 @@
 package com.example.sticknotes;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,52 +19,46 @@ public class MainActivity extends AppCompatActivity {
     //ListView listView;
     //SharedPreferences sharedPreferences;
 
-    ArrayList<String> notes = new ArrayList<>();
+     static ArrayList<String> notes = new ArrayList<String>();
+     static ArrayAdapter arrayAdapter;
 
-   // public void AddText(){
-   //     String s;
 
-    //}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ListView listView = findViewById(R.id.listView);
+       // sharedPreferences = this.getSharedPreferences("com.example.sticknotes", Context.MODE_PRIVATE);
+        notes.add("Tap to Add Your Notes");
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,notes);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(),NotesEditor.class);
+                intent.putExtra("itemid",position);
+                startActivity(intent);
+            }
+        });
 
-  /*  @Override
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.menu_bar,menu);
         return super.onCreateOptionsMenu(menu);
     }
 
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-         super.onOptionsItemSelected(item);
-         switch (item.getItemId()){
-             case R.id.add:
-             {
-                 //AddText();
-                 return true;
-             }
-             default:
-                 return false;
-         }
-    }
-*/
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        ListView listView = findViewById(R.id.listView);
-       // sharedPreferences = this.getSharedPreferences("com.example.sticknotes", Context.MODE_PRIVATE);
-        notes.add("Tap to Add Your Notes");
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,notes);
-        listView.setAdapter(arrayAdapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(getApplicationContext(),NotesEditor.class);
-                intent.putExtra("item-id",position);
-                startActivity(intent);
-            }
-        });
-
+        super.onOptionsItemSelected(item);
+        if(item.getItemId()== R.id.add){
+            Intent intent = new Intent(getApplicationContext(),NotesEditor.class);
+            startActivity(intent);
+            return true;
+        }
+        return false;
     }
 }
--- app/src/main/java/com/example/sticknotes/NotesEditor.java
+++ app/src/main/java/com/example/sticknotes/NotesEditor.java
@@ -2,13 +2,51 @@ package com.example.sticknotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 
 public class NotesEditor extends AppCompatActivity {
+     int itemId;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes_editor);
+        EditText editText = findViewById(R.id.editText);
+        Intent intent = getIntent();
+        itemId = intent.getIntExtra("itemid",-1);
+        //startActivity(intent);
+        if(itemId!=-1){
+            editText.setText(MainActivity.notes.get(itemId));
+        }
+        else {
+            MainActivity.notes.add(" ");
+            itemId = MainActivity.notes.size()-1;
+        }
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                    MainActivity.notes.set( itemId, String.valueOf(s));
+                    MainActivity.arrayAdapter.notifyDataSetChanged();
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+
+
+
     }
 }
